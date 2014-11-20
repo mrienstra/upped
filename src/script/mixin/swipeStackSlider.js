mrienstra/upped

commit f9d6fb1c7518c543a6e2489cc3c00c6fc2cf5a69

`app.jsx`: Added `handleGatheringsChange` & `handleRSVP`
`sideMenu.jsx`: Added "Gatherings"

`userListItem.jsx`: moved some methods into new `toggleStackListItem` mixin; Added generic `stackListItem` CSS class to allow broader CSS reuse

`remote.js`: Added `remote.parse.gatherings` methods (`getAll` & `rsvp`)

`heroes.jsx`: Moved some code into new `swipeStackSlider` mixin

`utils.js`: Added `momentFormatIfTime` method

`app.scss`: Changed `.userListItem` references to `.stackListItem`, to allow CSS reuse (from heroes to gatherings)

`src/script/screen/gatherings.jsx`, `src/script/component/gatheringListItem.jsx`: new screen, very similar to `heroes` screen

`src/script/mixin/swipeStackSlider.js`, `src/script/mixin/toggleStackListItem.js`: new mixins, abstracting out some of the code shared between `gatherings` & `heroes` (todo: abstract more!)

src/script/mixin/swipeStackSlider.js
@@ -1,0 +1,69 @@
+// Libs
+var swipeStack = require('../../lib/swipe-stack-0.1.js');
+
+var swipeStackSlider = {
+  sliderInit: function (window, document, btnNext, btnPrev, undefined) {
+    'use strict';
+
+    var that = this;
+
+    // Feature Test
+    if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
+
+      // SELECTORS
+      var sliders = document.querySelectorAll('[data-slider]');
+      var mySwipe = Array;
+
+
+      // EVENTS, LISTENERS, AND INITS
+
+      // Activate all sliders
+      Array.prototype.forEach.call(sliders, function (slider, index) {
+
+        // METHODS
+
+        // Handle next button
+        var handleNextBtn = function (event) {
+          event.preventDefault();
+          mySwipe[index].nextRight();
+        };
+
+        // Handle previous button
+        var handlePrevBtn = function (event) {
+          event.preventDefault();
+          mySwipe[index].nextLeft();
+        };
+
+        that.handleSliderPause = function(){
+          mySwipe[index].pause();
+        };
+
+        that.handleSliderUnpause = function(){
+          mySwipe[index].unpause();
+        };
+
+        // EVENTS, LISTENERS, AND INITS
+
+        // Activate Slider
+        mySwipe[index] = swipeStack(slider, {
+          callback: that.swipeStackCallback
+        });
+
+        // Toggle Previous & Next Buttons
+        if (btnNext) {
+          btnNext.addEventListener('click', handleNextBtn, false);
+        }
+        if (btnPrev) {
+          btnPrev.addEventListener('click', handlePrevBtn, false);
+        }
+      });
+
+      // Add class to HTML element to activate conditional CSS
+      window.setTimeout(function(){
+        document.documentElement.className += ' js-slider';
+      }, 10);
+    }
+  }
+};
+
+module.exports = swipeStackSlider;

src/script/screen/gatherings.jsx
@@ -1,0 +1,191 @@
+/** @jsx React.DOM */
+
+var React = require('react/addons');
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+
+// Libs
+var _ = require('lodash');
+
+// Mixins
+var swipeStackSlider = require('../mixin/swipeStackSlider.js');
+
+// Modules
+var pubSub = require('../pubSub.js');
+
+// Components
+var SideMenu = require('../component/sideMenu.jsx');
+var GatheringListItem = require('../component/gatheringListItem.jsx');
+
+var GatheringList = React.createClass({
+  mixins: [swipeStackSlider],
+  componentDidMount: function(){
+    var that = this;
+    this.swipeStackCallback = function (index, direction) {
+      console.log('GatheringList swipeStackCallback', this, arguments, that, that.props.gatherings);
+
+      pubSub.publish('gatheringList.current', {index: index + 1});
+
+      var targetGathering = that.props.gatherings[index];
+      var gathering = direction === 'left' ? 0 : 1;
+
+      if (index + 1 === that.props.gatherings.length) {
+        pubSub.publish('gatherings.hideButtons');
+      } else if (that.props.buttonsToTop) {
+        pubSub.publish('gatherings.toggleButtons', {expanded: false});
+        that.handleSliderUnpause();
+      }
+    };
+
+    var el = this.getDOMNode();
+    var btnNext = el.parentNode.querySelector('[data-slider-nav-next]');
+    var btnPrev = el.parentNode.querySelector('[data-slider-nav-prev]');
+    this.sliderInit(window, document, btnNext, btnPrev);
+  },
+  render: function() {
+    var gatheringNodes = this.props.gatherings.map(function (gathering, index) {
+      return <GatheringListItem key={index} gathering={gathering}></GatheringListItem>;
+    });
+
+    return (
+      <div className="content content-main">
+        <div className="subhead">
+          <h3>{this.props.name}</h3>
+        </div>
+
+        <div className="slider" data-slider>
+          <div className="slides">
+            {gatheringNodes}
+          </div>
+        </div>
+
+        <div className="done">
+          <h3>Galaxy Explored</h3>
+          <p>You’ve seen all upcoming gatherings.</p>
+        </div>
+      </div>
+    );
+  }
+});
+
+var GatheringsScreen = React.createClass({
+  getInitialState: function(){
+    return {
+      buttonsToTop: false,
+      hideButtons: false,
+      currentIndex: 0,
+      gatherings: void 0
+    };
+  },
+  handlePromiseThen: function (gatherings) {
+    console.log('GatheringsScreen gatheringsPromise success handlePromiseThen', gatherings, this.props.remote.user.userData.gatherings, this.pendingGatherings);
+
+    var userGatherings = this.props.remote.user.userData.gatherings;
+
+    /*
+    window.removeEventListener('getParseGatheringsSuccess', this.handlePromiseThen);
+    if (!userGatherings) {
+      this.pendingGatherings = gatherings;
+      window.addEventListener('getParseGatheringsSuccess', this.handlePromiseThen);
+      console.log('GatheringsScreen.handlePromiseThen: Waiting for getParseGatheringsSuccess...');
+      return;
+    } else if (gatherings instanceof window.Event) {
+      gatherings = this.pendingGatherings;
+    }
+    */
+
+    /*
+    gatherings = gatherings.filter(function (gathering) {
+      return (
+        !_.contains(gatherings.unchosenOnes, gathering.id) && // Don't show gatherings we've already unchosen
+        !_.contains(gatherings.chosenOnes, gathering.id) // Don't show gatherings we've already chosen
+      );
+    });
+
+    gatherings = _.sortBy(gatherings, function (gathering) {
+      return !_.contains(gatherings.chosenBy, gathering.id); // Show `chosenBy` gatherings first
+    });
+    */
+
+    this.setState({
+      gatherings: gatherings
+    });
+  },
+  handlePromise: function (gatheringsPromise) {
+    gatheringsPromise.then(
+      this.handlePromiseThen,
+      function (response) {
+        alert('GatheringsScreen gatheringsPromise failed!');
+        console.warn('bad', response);
+      }
+    );
+  },
+  componentWillMount: function(){
+    console.log('GatheringsScreen.componentWillMount', this, arguments);
+
+    var that = this;
+
+    var gatheringsPromise = this.props.getGatherings();
+
+    this.handlePromise(gatheringsPromise);
+
+    pubSub.unsubscribe('gatheringList.current', this.updateCurrentIndex);
+    pubSub.subscribe('gatheringList.current', this.updateCurrentIndex);
+
+    pubSub.unsubscribe('gatherings.toggleButtons', this.toggleButtons);
+    pubSub.subscribe('gatherings.toggleButtons', this.toggleButtons);
+
+    pubSub.unsubscribe('gatherings.hideButtons', this.hideButtons);
+    pubSub.subscribe('gatherings.hideButtons', this.hideButtons);
+  },
+  hideButtons: function(){
+    this.setState({hideButtons: true});
+  },
+  toggleButtons: function (channel, data) {
+    this.setState({buttonsToTop: data.expanded});
+    if (data.expanded) {
+      this.handleSliderPause();
+    } else {
+      this.handleSliderUnpause();
+    }
+  },
+  updateCurrentIndex: function (channel, data) {
+    this.setState({currentIndex: data.index});
+  },
+  render: function(){
+    var that = this;
+
+    var gatheringList;
+    if (this.state.gatherings === void 0) {
+      gatheringList = (
+        <div className="content content-main">
+          <span className="icon ion-loading-d"></span>
+        </div>
+      );
+    } else {
+      gatheringList = <GatheringList gatherings={this.state.gatherings} handleChoice={this.props.handleChoice} buttonsToTop={this.state.buttonsToTop}></GatheringList>
+    }
+
+    var handleToggleDetails = function(){
+      pubSub.publish('gatherings.toggleStackListItem.' + that.state.currentIndex);
+    };
+
+    return (
+      <div>
+        <header className="bar bar-nav">
+          <a className="btn btn-link btn-nav pull-left" onTouchEnd={this.props.handleBack} data-transition="slide-out"><span className="icon icon-bars"></span></a>
+          <h1 className="title">Gatherings</h1>
+        </header>
+
+        {gatheringList}
+
+        <div className={'round-buttons' + (this.state.hideButtons ? ' hide' : '') + (this.state.buttonsToTop ? ' top' : '')}>
+          <a data-slider-nav-prev className="icon icon-button button-no pull-left"></a>
+          <button onTouchEnd={handleToggleDetails} className="icon icon-button button-info"></button>
+          <a data-slider-nav-next className="icon icon-button button-yes pull-right"></a>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = GatheringsScreen;

src/script/screen/heroes.jsx
@@ -5,70 +5,9 @@ var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 // Libs
 var _ = require('lodash');
-var swipeStack = require('../../lib/swipe-stack-0.1.js');
-var swipeStackCallback;
-var handlePause;
-var handleUnpause;
-var sliderInit = function (window, document, btnNext, btnPrev, undefined) {
-  'use strict';
 
-  // Feature Test
-  if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
-
-    // SELECTORS
-    var sliders = document.querySelectorAll('[data-slider]');
-    var mySwipe = Array;
-
-
-    // EVENTS, LISTENERS, AND INITS
-
-    // Activate all sliders
-    Array.prototype.forEach.call(sliders, function (slider, index) {
-
-      // METHODS
-
-      // Handle next button
-      var handleNextBtn = function (event) {
-        event.preventDefault();
-        mySwipe[index].nextRight();
-      };
-
-      // Handle previous button
-      var handlePrevBtn = function (event) {
-        event.preventDefault();
-        mySwipe[index].nextLeft();
-      };
-
-      handlePause = function(){
-        mySwipe[index].pause();
-      };
-
-      handleUnpause = function(){
-        mySwipe[index].unpause();
-      };
-
-      // EVENTS, LISTENERS, AND INITS
-
-      // Activate Slider
-      mySwipe[index] = swipeStack(slider, {
-        callback: swipeStackCallback
-      });
-
-      // Toggle Previous & Next Buttons
-      if (btnNext) {
-        btnNext.addEventListener('click', handleNextBtn, false);
-      }
-      if (btnPrev) {
-        btnPrev.addEventListener('click', handlePrevBtn, false);
-      }
-    });
-
-    // Add class to HTML element to activate conditional CSS
-    window.setTimeout(function(){
-      document.documentElement.className += ' js-slider';
-    }, 10);
-  }
-};
+// Mixins
+var swipeStackSlider = require('../mixin/swipeStackSlider.js');
 
 // Modules
 var pubSub = require('../pubSub.js');
@@ -78,9 +17,10 @@ var SideMenu = require('../component/sideMenu.jsx');
 var UserListItem = require('../component/userListItem.jsx');
 
 var UserList = React.createClass({
+  mixins: [swipeStackSlider],
   componentDidMount: function(){
     var that = this;
-    swipeStackCallback = function (index, direction) {
+    this.swipeStackCallback = function (index, direction) {
       console.log('UserList swipeStackCallback', this, arguments, that, that.props.users);
 
       pubSub.publish('userlist.current', {index: index + 1});
@@ -99,14 +39,14 @@ var UserList = React.createClass({
         pubSub.publish('heroes.hideButtons');
       } else if (that.props.buttonsToTop) {
         pubSub.publish('heroes.toggleButtons', {expanded: false});
-        handleUnpause();
+        that.handleSliderUnpause();
       }
     };
 
     var el = this.getDOMNode();
     var btnNext = el.parentNode.querySelector('[data-slider-nav-next]');
     var btnPrev = el.parentNode.querySelector('[data-slider-nav-prev]');
-    sliderInit(window, document, btnNext, btnPrev);
+    this.sliderInit(window, document, btnNext, btnPrev);
   },
   render: function() {
     var userNodes = this.props.users.map(function (user, index) {
@@ -211,9 +151,9 @@ var ChooseScreen = React.createClass({
   toggleButtons: function (channel, data) {
     this.setState({buttonsToTop: data.expanded});
     if (data.expanded) {
-      handlePause();
+      this.handleSliderPause();
     } else {
-      handleUnpause();
+      this.handleSliderUnpause();
     }
   },
   updateCurrentIndex: function (channel, data) {
@@ -265,7 +205,7 @@ var ChooseScreen = React.createClass({
     }
 
     var handleToggleDetails = function(){
-      pubSub.publish('toggleDataUserListItem.' + that.state.currentIndex);
+      pubSub.publish('heroes.toggleStackListItem.' + that.state.currentIndex);
     };
 
     return (
@@ -290,7 +230,7 @@ var ChooseScreen = React.createClass({
           </div>
         </div>
 
-        <SideMenu id="sideMenu" handleMatchesChange={this.props.handleMatchesChange} handleMyProfileChange={this.props.handleMyProfileChange} handleLogOut={this.props.handleLogOut} />
+        <SideMenu id="sideMenu" handleMatchesChange={this.props.handleMatchesChange} handleMyProfileChange={this.props.handleMyProfileChange} handleGatheringsChange={this.props.handleGatheringsChange} handleLogOut={this.props.handleLogOut} />
 
       </div>
     );

src/script/app.jsx
@@ -57,7 +57,7 @@ var handleHeroesChange = function(){
   var handleMyProfileChange = handleProfileChange.bind(null, remote.user.userData, true);
 
   app.screens.addScreen(
-    <HeroesScreen remote={remote} getUsers={getUsers} handleChoice={handleChoice} handleMyProfileChange={handleMyProfileChange} handleMatchesChange={handleMatchesChange} handleLogOut={handleLogOut}></HeroesScreen>
+    <HeroesScreen remote={remote} getUsers={getUsers} handleChoice={handleChoice} handleMyProfileChange={handleMyProfileChange} handleMatchesChange={handleMatchesChange} handleGatheringsChange={handleGatheringsChange} handleLogOut={handleLogOut}></HeroesScreen>
   );
 };
 
@@ -72,6 +72,18 @@ var handleLocationChange = function (props) {
 
   app.screens.addScreen(
     <PostsScreen photoURL={props.photoURL} name={props.name} checkin={props.checkin} distance={props.distance} address1={props.address1} address2={props.address2} promotion={props.promotion} posts={props.posts} fbId={props.fbId} handleBack={handleBack} user={remote.user} handleCheckInOut={handleCheckInOut} handleCreatePostOrComment={handleCreatePostOrComment} handleProfileChange={handleProfileChange} handlePostChange={handlePostChange} getPosts={getPosts} handleLike={handleLike} handleMatchesChange={handleMatchesChange} handleMyProfileChange={handleMyProfileChange} handleLogOut={handleLogOut}></PostsScreen>
+  );
+};
+
+var handleGatheringsChange = function(){
+  console.log('handleGatheringsChange', this, arguments);
+
+  var GatheringsScreen = require('./screen/gatherings.jsx');
+
+  var getGatherings = remote.parse.gatherings.getAll;
+
+  app.screens.addScreen(
+    <GatheringsScreen remote={remote} getGatherings={getGatherings} handleChoice={handleRSVP} handleBack={handleBack}></GatheringsScreen>
   );
 };
 
@@ -128,6 +140,12 @@ var handleChoice = function (chosenId, choice) {
   console.log('handleChoice', this, arguments, remote.parse.userData);
 
   return remote.parse.choice.set(chosenId, choice);
+};
+
+var handleRSVP = function (chosenId, choice) {
+  console.log('handleRSVP', this, arguments, remote.parse.userData);
+
+  return remote.parse.gatherings.rsvp(chosenId, choice);
 };
 
 var handleCreatePostOrComment = function (isPostsOrComments, msg, pictureDataURI, successCallback, failureCallback) {

src/script/component/gatheringListItem.jsx
@@ -1,0 +1,34 @@
+/** @jsx React.DOM */
+
+var React = require('react/addons');
+
+// Mixins
+var toggleStackListItem = require('../mixin/toggleStackListItem.js');
+
+// Modules
+var utils = require('../utils');
+
+var GatheringListItem = React.createClass({
+  mixins: [toggleStackListItem],
+  render: function() {
+    var img = this.props.gathering.photoURL ? <img src={this.props.gathering.photoURL}/> : '';
+
+    var date = utils.momentFormatIfTime(this.props.gathering.date);
+    var fromNow = utils.momentFromNowIfTime(this.props.gathering.date);
+
+    return (
+      <div className="stackListItem gatheringListItem" onTouchEnd={this.handleToggleDetails}>
+        {img}
+        <div className="summary">
+          <div className="nameAndSkillCount">{date}</div>
+          <div className="location">{this.props.gathering.location} {fromNow}</div>
+        </div>
+        <div className={'details' + (this.state.expanded ? ' show' : '')}>
+          <div className="blurb">{this.props.gathering.blurb}</div>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = GatheringListItem;

src/script/component/sideMenu.jsx
@@ -9,6 +9,7 @@ var SideMenu = React.createClass({
   propTypes: {
     handleMatchesChange: React.PropTypes.func.isRequired,
     handleBack: React.PropTypes.func, // Not required
+    handleGatheringsChange: React.PropTypes.func.isRequired,
     handleMyProfileChange: React.PropTypes.func.isRequired,
     handleLogOut: React.PropTypes.func.isRequired,
     id: React.PropTypes.string.isRequired
@@ -21,6 +22,11 @@ var SideMenu = React.createClass({
             <li className="table-view-cell">
               <a onTouchEnd={this.props.handleMyProfileChange}>
                 <h4><span className="icon ion-person"></span>Profile</h4>
+              </a>
+            </li>
+            <li className="table-view-cell">
+              <a onTouchEnd={this.props.handleGatheringsChange}>
+                <h4><span className="icon ion-search"></span>Gatherings</h4>
               </a>
             </li>
             <li className="table-view-cell">

src/script/component/userListItem.jsx
@@ -2,30 +2,11 @@
 
 var React = require('react/addons');
 
-// Modules
-var pubSub = require('../pubSub.js');
+// Mixins
+var toggleStackListItem = require('../mixin/toggleStackListItem.js');
 
 var UserListItem = React.createClass({
-  getInitialState: function(){
-    return {
-      expanded: !!this.props.fromMenu
-    };
-  },
-  componentWillMount: function(){
-    pubSub.unsubscribe('toggleDataUserListItem.' + this.props.key, this.handleToggleDetails);
-    pubSub.subscribe('toggleDataUserListItem.' + this.props.key, this.handleToggleDetails);
-  },
-  handleToggleDetails: function(){
-    if (!this.props.fromMenu) {
-      console.log('pubsub.subscribe toggleDataUserListItem.' + this.props.key + ' handleToggleDetails', this.state.expanded);
-
-      pubSub.publish('heroes.toggleButtons', {expanded: !this.state.expanded});
-
-      this.setState({expanded: !this.state.expanded});
-    } else {
-      console.log('pubsub.subscribe toggleDataUserListItem.' + this.props.key + ' handleToggleDetails, ignoring because this.props.fromMenu', this.props.fromMenu);
-    }
-  },
+  mixins: [toggleStackListItem],
   render: function() {
     var img = this.props.user.photoURL ? <img src={this.props.user.photoURL}/> : '';
 
@@ -45,7 +26,7 @@ var UserListItem = React.createClass({
     });
 
     return (
-      <div className="userListItem" onTouchEnd={this.handleToggleDetails}>
+      <div className="stackListItem userListItem" onTouchEnd={this.handleToggleDetails}>
         {img}
         <div className={'summary' + (this.state.expanded ? ' hide' : '')}>
           <div className="nameAndSkillCount">{this.props.user.name}</div>

commit c87f806576fbc62c15b45ca44194625acda3288b

Fix for bug introduced in f9d6fb1 (2nd to last commit)

src/script/mixin/swipeStackSlider.js
@@ -2,6 +2,13 @@
 var swipeStack = require('../../lib/swipe-stack-0.1.js');
 
 var SwipeStackSliderMixin = {
+  componentWillReceiveProps: function (nextProps) {
+    if (nextProps.buttonsToTop && !this.props.buttonsToTop) {
+      this.handleSliderPause();
+    } else if (!nextProps.buttonsToTop && this.props.buttonsToTop) {
+      this.handleSliderUnpause();
+    }
+  },
   sliderInit: function (window, document, btnNext, btnPrev, undefined) {
     'use strict';
 

src/script/screen/gatherings.jsx
@@ -32,7 +32,6 @@ var GatheringList = React.createClass({
         pubSub.publish('gatherings.hideButtons');
       } else if (that.props.buttonsToTop) {
         pubSub.publish('gatherings.toggleButtons', {expanded: false});
-        that.handleSliderUnpause();
       }
     };
 
@@ -142,11 +141,6 @@ var GatheringsScreen = React.createClass({
   },
   toggleButtons: function (channel, data) {
     this.setState({buttonsToTop: data.expanded});
-    if (data.expanded) {
-      this.handleSliderPause();
-    } else {
-      this.handleSliderUnpause();
-    }
   },
   updateCurrentIndex: function (channel, data) {
     this.setState({currentIndex: data.index});

src/script/screen/heroes.jsx
@@ -39,7 +39,6 @@ var UserList = React.createClass({
         pubSub.publish('heroes.hideButtons');
       } else if (that.props.buttonsToTop) {
         pubSub.publish('heroes.toggleButtons', {expanded: false});
-        that.handleSliderUnpause();
       }
     };
 
@@ -150,11 +149,6 @@ var ChooseScreen = React.createClass({
   },
   toggleButtons: function (channel, data) {
     this.setState({buttonsToTop: data.expanded});
-    if (data.expanded) {
-      this.handleSliderPause();
-    } else {
-      this.handleSliderUnpause();
-    }
   },
   updateCurrentIndex: function (channel, data) {
     this.setState({currentIndex: data.index});