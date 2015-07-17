0b1c7de3a79929bfaa4df66c14cb002651f0614f
Erik Johansson <clearhythm@hotmail.com>
2015-07-16 17:31:41 -0700

    fixed up the how it works page

--- src/script/screen/howItWorks.jsx
+++ src/script/screen/howItWorks.jsx
@@ -32,9 +32,35 @@ var FeedbackScreen = React.createClass({
           <h1 className="title">How it Works</h1>
         </div>
 
-        <div className="scroll-content overflow-scroll has-header">
-
-          <iframe className="fullframe" src="http://www.paywithsushi.com/how-it-works"></iframe>
+        <div className="scroll-content overflow-scroll has-header padding howitworks">
+          <h3>How Does Pay with Sushi Work?</h3>
+          <p>Pay with Sushi is a mobile payment platform that lets you pay for the stuff your business needs with your goods and services.
+          We connect you with great local businesses, keep track of who owes who and have your back if anything goes wrong. Here&#8217;s how it works:</p>
+          <ol>
+            <li>
+              <h4><div className="counter">1</div>Swipe through local businesses to find a match</h4>
+              <img src="http://static1.squarespace.com/static/552367eae4b0fb7c9860d7af/t/55a6db3ce4b0a494fbb3aa27/1436999778654/" alt="Step one" />
+              <p>We have 1000&#8217;s of great businesses near you who want what you have &mdash;
+              web designers, plumbers, photographers, massage therapists, yoga teachers, or even sumo masters.
+              When you find one you want too, just click &#8220;want&#8221;. When there&#8217;s a match we&#8217;ll let you know!
+              (Don&#8217;t worry, you can figure out the details in the next step!)</p>
+            </li>
+            <li>
+              <h4><div className="counter">2</div>Agree on the value you want to exchange</h4>
+              <img src="http://static1.squarespace.com/static/552367eae4b0fb7c9860d7af/t/55a6db3ce4b0a494fbb3aa28/1435988371661/" alt="Step two" />
+              <p>Once you&#8217;ve found a match, settle on the cash value you want to exchange.
+              Most transactions are about $250.</p>
+            </li>
+            <li>
+              <h4><div className="counter">3</div>Track it like cash</h4>
+              <img src="http://static1.squarespace.com/static/552367eae4b0fb7c9860d7af/t/55a6db3ce4b0a494fbb3aa29/1435990231806/" alt="Step three" />
+              <p>We keep track of everything for you. Check in to our app any time to see how much credit you have remaining, redeem offers, and track payment activity. Simple.</p>
+            </li>
+          </ol>
+          <hr/>
+          <h3>That&#8217;s it!</h3>
+          <p className="center">Have questions? Get in touch with us and a real live person will get back to you directly!<br/>
+          <a href="mailto:sushi@paywithsushi.com">sushi@paywithsushi.com</a></p>
         </div>
       </div>
     );
